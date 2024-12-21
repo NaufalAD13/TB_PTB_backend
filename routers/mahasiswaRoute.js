3b5db923f808a000df4818ebad6a7c8c09b9dd7c
Naufal Adli Dhiaurrahman <naufaladlidhiaurrahman@gmail.com>
2024-12-21 21:13:10 +0700

    login

--- routers/mahasiswaRoute.js
+++ routers/mahasiswaRoute.js
@@ -2,16 +2,17 @@ const express = require("express");
 const router = express.Router();
 const mahasiswaController = require('../controllers/mahasiswaController');
 const upload = require("../middleware/upload")
+const {authenticateToken} = require("../middleware/authToken");
 
 // Existing routes
-router.get('/namaMahasiswa', mahasiswaController.lihatMahasiswa);
-router.get('/namaMahasiswa/:id_mahasiswa', mahasiswaController.lihatDetail);
+router.get('/namaMahasiswa', authenticateToken, mahasiswaController.lihatMahasiswa);
+router.get('/namaMahasiswa/:id_mahasiswa', authenticateToken, mahasiswaController.lihatDetail);
 
 
 // New pengajuan route
-router.get('/namaMahasiswa/:id_mahasiswa/pengajuan', mahasiswaController.lihatPengajuan);
-router.post('/seminar', upload.single('surat'), mahasiswaController.createSeminar)
+router.get('/namaMahasiswa/:id_mahasiswa/pengajuan', authenticateToken, mahasiswaController.lihatPengajuan);
+router.post('/seminar', upload.single('surat'), authenticateToken, mahasiswaController.createSeminar)
 
-router.put("/pengajuan/:id/status", mahasiswaController.updateStatusPengajuan);
+router.put("/pengajuan/:id/status", authenticateToken, mahasiswaController.updateStatusPengajuan);
 
 module.exports = router;
